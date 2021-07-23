6bec243ea7f03ce7be9dc82d2f388c62de5569aa
Doriane <berdalado@gmail.com>
2021-07-23 11:09:30 +0200

    title in the bookdetailsPage

--- src/screens/BookDetailsPage.js
+++ src/screens/BookDetailsPage.js
@@ -1,16 +1,19 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { BookContext } from "../contexts/bookContext";
+import { useContext } from "react";
 import { useParams } from "react-router";
 
-export default function BookDetailsPage({ bookList }) {
+export default function BookDetailsPage() {
+  const { bookList } = useContext(BookContext);
   const { id } = useParams();
   const bookId = parseInt(id, 10);
-  console.log(bookId);
 
-  const selectedBook = bookList[bookId];
   return (
-    <div>
-      <h1>{selectedBook.displayTitle}</h1>
-    </div>
+    <>
+      <h1>
+        {bookList
+          .filter((book) => book.id === bookId)
+          .map((id) => id.displayTitle)}
+      </h1>
+    </>
   );
 }
